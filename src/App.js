fdba6b882d6792454727bd295613cdee4119e6ee
Павел Зотов <paulzotoff@gmail.com>
2019-01-13 14:54:36 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
-import Button from 'react-bootstrap/lib/Button';
+
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
+
+
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Main from './components/Main';
+
+const url = process.env.PUBLIC_URL+'/img/bg.jpg';
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 49px;
+  background-color: #242424;
+  opacity: 0.8;
+`
+
+const MenuWrapper = styled.div`
+  height: 89px;
+  padding-top: 20px;
+`
+const MainWrapper = styled.main`
+  height: 600px;
+  padding-top: 130px;
+  background: url(${url}) no-repeat;
+  background-size: cover;
+`
 
 class App extends Component {
   render() {
     return (
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <p>
-      //       Edit <code>src/App.js</code> and save to reload.
-      //     </p>
-      //     <a
-      //       className="App-link"
-      //       href="https://reactjs.org"
-      //       target="_blank"
-      //       rel="noopener noreferrer"
-      //     >
-      //       Learn React
-      //     </a>
-      //   </header>
-      // </div>
-      <div className="wrapper">
-        <SayName name="Pavel" lastname="Petrov" link="test.com"/>
-        <SayName name="Pavel" lastname="Petrov" link="test.com"/>
-        <SayName name="Pavel" lastname="Petrov" link="test.com"/>
+      <div className='App'>
+        <HeaderWrapper>
+          <Grid>
+            <Header />
+          </Grid>
+        </HeaderWrapper>
+        <MenuWrapper>
+          <Grid>
+            <Menu />
+          </Grid>
+        </MenuWrapper>
+        <MainWrapper>
+          <Grid>
+            <Main />
+          </Grid>
+        </MainWrapper>
+        
       </div>
     );
   }
-}
-
-function SayName(props){
-  return (
-      <div>
-          <h1>Name: {props.name}, Surname: {props.lastname}</h1>
-          <a href={props.link}>Link</a>
-      </div>
-  )
 }
 
 export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components';
+
+const Repair = styled.div`
+    color: #000;
+    font-size: 40px;
+    font-weight: 700;
+    line-height: 30px;
+
+    span {
+        display: block;
+        font-size: 24px;
+        line-height: 30px;
+    }
+`
+const LightText = styled.div`
+    color: #000;
+    font-size: 18px;
+    line-height: 28px;
+    font-weight: 300;
+    margin-top: 54px;
+`
+const CallBtn = styled.button`
+    width: 247px;
+    height: 67px;
+    background-color: #ffa14b;
+    border-radius: 30px;
+    color: #ffffff;
+    font-size: 18px;
+    font-weight: bold;
+    margin-top: 30px;
+`
+
+class Main extends React.Component {
+    render() {
+        return (
+            <Row>
+                <Col lg={5}>
+                    <Repair>
+                        Качественный ремонт
+                        <span>iPhone за 35 минут и гарантия 1 год</span>
+                    </Repair>
+                    <LightText>
+                        Оставьте заявку на бесплатную диагностику без очереди, и получите защитное стекло стоимостью 1000 рублей с установкой в подарок!
+                    </LightText>
+                    <CallBtn>Отправить заявку</CallBtn>
+                </Col>
+                <Col lg={6} lgOffset={1}>
+
+                </Col>
+            </Row>
+        );
+    }
+}
+
+export default Main;
